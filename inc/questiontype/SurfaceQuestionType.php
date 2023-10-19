0de756c9e47c978d3c44920705eb12406970232c
ljacqu <ljacqu@users.noreply.github.com>
2023-10-19 22:04:57 +0200

    Use the right text keys for surface questions

--- inc/questiontype/SurfaceQuestionType.php
+++ inc/questiontype/SurfaceQuestionType.php
@@ -2,16 +2,15 @@
 
 class SurfaceQuestionType extends CountryBasedQuestionType {
 
-
   function generateKey(Question $question): string {
     return 'srf_' . substr(md5($question->question), 0, 16);
   }
 
   protected function getQuestionTextKey(): string {
-    return 'population_question';
+    return 'surface_question';
   }
 
   protected function getResolutionTextKey(): string {
-    return 'population_resolution';
+    return 'surface_resolution';
   }
 }
